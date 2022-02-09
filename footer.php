674a0eb95e93e870e3d6885b2d2a73b6a6b5deb4
Pierre-Olivier Clerson <poclerson@gmail.com>
2022-02-09 17:39:32 -0500

    Tout le cours 2 de la semaine 3

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="site__footer">
+    <div class="footer__info">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat quaerat esse rerum illo accusantium eaque obcaecati quos, aperiam dolore totam tempora minima quo accusamus quod asperiores molestias magni iure possimus.
+    </div>
+    <div class="footer__contact">
+        <ul>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li>Dolor</li>
+            <li>Sit amet</li>
+        </ul>
+    </div>
+    <div class="footer__adresse">
+        3800 Sherbrooke Est, Montréal
+    </div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+    /* Enqueue */
+    function cidw_4w4_enqueue() {
+        wp_enqueue_style('  style_css', 
+                            get_template_directory_uri() . '/style.css', 
+                            array(), filemtime(get_template_directory() . '/style.css'), 
+                            false);
+
+        # Nunito
+        wp_enqueue_style('  style_css',
+                            "https://fonts.googleapis.com/css2?family=Nunito:wght@100&display=swap",
+                            false);
+
+        # Josefin Sans
+        wp_enqueue_style('  style_css',
+                            "https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap",
+                            false);
+    } 
+
+    add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
+
+    /* Enregistrer le menu */
+    function cidw_4w4_enregistrer_menu() {
+        register_nav_menu('principal', __('Menu principal', 'cidw_4w4'));
+    }
+
+    add_action('after_setup_theme', 'cidw_4w4_enregistrer_menu');
+
+    /* Filtrer le menu */
+    function cidw_4w4_filtrer_menu($objet) {
+        /* Afficher le contenu d'un objet */
+        # => sert à accéder a la valeur d'un tableau associatif
+        foreach($objet as $cle => $valeur) {
+            # -> sert à accéder à la valeur d'un objet
+            # wp_trim_words([objet], [nombre de mots a garde], [terminer par])
+            $valeur->title = wp_trim_words($valeur->title, 4, "...");
+        }   
+
+        return $objet;
+    }
+
+    add_filter("wp_nav_menu_objects", "cidw_4w4_filtrer_menu")
+?>
