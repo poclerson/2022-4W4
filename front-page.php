35be9a46b362003ee4a7ea61ff7aee51f975e2c3
Pierre-Olivier Clerson <poclerson@gmail.com>
2022-03-16 17:39:25 -0400

    Animation de la page d'accueil

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
-<main class="site__main">
-    <h1 class="site__main__titre">Accueil</h1>
+<main class="site__main home">
+    <h1 class="site__main__titre home__titre">Accueil</h1>
     <?php if (have_posts()): ?>
         <?php
             the_title();
